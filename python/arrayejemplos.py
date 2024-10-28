8cf437b70524660f7b9af85afa333bee8a5bc582
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2024-10-28 10:03:48 +0100

    subir ejemplos
    subimos los ejemplos de array

---
+++ python/arrayejemplos.py
@@ -0,0 +1,17 @@
+#ejemplo1
+import numpy as np
+
+calificaciones_1=[80,90,70,85]
+calificaciones_2=[75,85,95,80]
+
+array1= np.array(calificaciones_1)
+array2=np.array(calificaciones_2)
+
+total=array1+array2
+print(total)
+
+#ejemplo2
+import numpy as np
+
+matriz_identidad=np.eye(3)
+print(matriz_identidad)
