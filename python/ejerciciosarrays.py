1e6b0da926d641d9972e58ce315ae9d6c9fc7417
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2024-10-28 13:17:59 +0100

    ejercicios array
    añadimos los ejercicios de array

---
+++ python/ejerciciosarrays.py
@@ -0,0 +1,35 @@
+#ejercicio1
+#import numpy as np
+
+#array1=np.array([1,2,3,4,5])
+#array2=np.array([7, 42, 19, 88, 33])
+#suma= array1+array2
+#print(suma)
+
+#ejercicio2s
+#import numpy as np
+
+#array=np.array([12, 45, 27, 63, 9, 74])
+#multiplicacion=array*3
+#print(multiplicacion)
+
+#ejercicio3
+#import numpy as np
+
+#array=([3, 56, 21, 78,16, 58, 24, 83, 7, 3])
+#promedio=np.mean(array)
+#print(promedio)
+
+#ejercicio4
+#import numpy as np
+
+#array=np.array([4, 67, 23, 81, 15, 39, 90, 11])
+#array2= array[array > 5]
+#print(array2)
+
+#ejercicio5
+import numpy as np
+
+array=np.array([8, 54, 31, 76, 22])
+cuatrado=array **2
+print(cuatrado)
