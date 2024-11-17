1461c2b1508c8a8278474cecceb49c0fbe986808
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2024-11-17 14:44:22 +0100

    ejercicio18
    ejercicio 18 terminado

--- ejercicio18/inicio.py
+++ ejercicio18/inicio.py
@@ -1,78 +1,80 @@
-import funciones
+#inicio
 
-conexion=funciones.conectar("SUPERMERCADO")
+import funciones # Importamos funciones.py
 
-while True:
-    funciones.menu_principal()
-    opcion=int(input("introduce la tabla: "))
-    if opcion==1:
-        funciones.menu_operacion()
+conexion=funciones.conectar("SUPERMERCADO")# Hacemos que se conecte a la base de datos supermercado 
+
+while True:# Creamos un while true para el usuario elija la opciones que quiera y selecione que hacer para cada tabla
+    funciones.menu_principal()# Pone el menu de tablas que hay en la base de datos
+    opcion=int(input("introduce la tabla: "))# Introduce la tabla que vamos a modificar
+    if opcion==1:# Tabla categoria
+        funciones.menu_operacion()# Menu Operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
-        if elecion==1:
+        if elecion==1:# opcion de crear
             funciones.crear_categoria(conexion)
-        elif elecion==2:
+        elif elecion==2:# opcion de listar
             funciones.leer_categoria(conexion)
-        elif elecion==3:
+        elif elecion==3:# opcion de actualizar
             funciones.actualizar_categoria(conexion)
-        elif elecion==4:
+        elif elecion==4:# opcion de eliminar
             funciones.eliminar_categoria(conexion)
-        elif elecion==5:
+        elif elecion==5:# Salir
             print("Saliendo de la gestion de categorias. Hasta pronto")
             break
-    elif opcion==2:
-        funciones.menu_operacion()
+    elif opcion==2:# Tabla detalle
+        funciones.menu_operacion()# Menu Operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
-        if elecion==1:
+        if elecion==1:# opcion de crear
             funciones.crear_detalle(conexion)
-        elif elecion==2:
+        elif elecion==2:# opcion de listar
             funciones.leer_detalle(conexion)
-        elif elecion==3:
+        elif elecion==3:# opcion de actualizar
             funciones.actualizar_detalle(conexion)
-        elif elecion==4:
+        elif elecion==4:# opcion de eliminar
             funciones.eliminar_detalles(conexion)
-        elif elecion==5:
+        elif elecion==5:# Salir
             print("Saliendo de la gestion de detalle. Hasta pronto")
             break
-    elif opcion==3:
-        funciones.menu_operacion()
+    elif opcion==3:# Table pedido
+        funciones.menu_operacion()# Menu Operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
-        if elecion==1:
+        if elecion==1:# opcion de crear
             funciones.crear_pedidos(conexion)
-        elif elecion==2:
+        elif elecion==2:# opcion de listar
             funciones.leer_pedidos(conexion)
-        elif elecion==3:
+        elif elecion==3:# opcion de actualizar
             funciones.actualizar_pedido(conexion)
-        elif elecion==4:
+        elif elecion==4:# opcion de eliminar
             funciones.eliminar_pedido(conexion)
-        elif elecion==5:
+        elif elecion==5:# Salir
             print("Saliendo de la gestion de pedido. Hasta pronto")
             break
-    elif opcion==4:
-        funciones.menu_operacion()
+    elif opcion==4:# Tabla cliente
+        funciones.menu_operacion()# Menu Operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
-        if elecion==1:
+        if elecion==1:# opcion de crear
             funciones.crear_cliente(conexion)
-        elif elecion==2:
+        elif elecion==2:# opcion de listar
             funciones.leer_cliente(conexion)
-        elif elecion==3:
+        elif elecion==3:# opcion de actualizar
             funciones.actualizar_cliente(conexion)
-        elif elecion==4:
+        elif elecion==4:# opcion de eliminar
             funciones.eliminar_cliente(conexion)
-        elif elecion==5:
+        elif elecion==5:# Salir
             print("Saliendo de la gestion de cliente. Hasta pronto")
             break
-    elif opcion==5:
-        funciones.menu_operacion()
+    elif opcion==5:# Tabla Producto
+        funciones.menu_operacion()# Menu Operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
-        if elecion==1:
+        if elecion==1:# opcion de crear
             funciones.crear_producto(conexion)
-        elif elecion==2:
+        elif elecion==2:# opcion de listar
             funciones.leer_producto(conexion)
-        elif elecion==3:
+        elif elecion==3:# opcion de actualizar
             funciones.actualizar_producto(conexion)
-        elif elecion==4:
+        elif elecion==4:# opcion de eliminar
             funciones.eliminar_producto(conexion)
-        elif elecion==5:
+        elif elecion==5:# Salir
             print("Saliendo de la gestion de producto. Hasta pronto")
             break
     elif opcion==6:
