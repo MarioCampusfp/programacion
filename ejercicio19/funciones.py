7d85a009860fb5484fbd48875d16551fe9cd3684
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2024-11-20 14:04:16 +0100

    19
    ejercicio 19 acabado

--- ejercicio19/funciones.py
+++ ejercicio19/funciones.py
@@ -39,7 +39,7 @@ def registrar_cliente(conexion):# Creamos la funcion registrar cliente que nos s
         nombre=input("introduce el nombre del nuevo cliente: ")
         edad=int(input("introduce la edad del nuevo cliente: "))
         membresia=int(input("introduce el tipo de menbresia del nuevo cliente"))
-        consulta="""INSERT INTO clientes VALUES(%s,%s,%s,%s)""" # Comando que se ejecutara en la base de datos para la creacion del cliente
+        consulta="""INSERT INTO cliente VALUES(%s,%s,%s,%s)""" # Comando que se ejecutara en la base de datos para la creacion del cliente
         cursor.execute(consulta,(id, nombre, edad, membresia)) # Datos que llevara el comando al ejecutarse
         conexion.commit()
         print(f"el cliente {nombre} se a creado correctamente") # Print de que el cliente se a creado correctamente
@@ -61,8 +61,8 @@ def eliminar_clientes(conexion): # Creamos la funcion para eliminar al los clien
     try:# ponemos un try para la localizacion de errores
         cursor=conexion.cursor() # Cursor para conectar a la base de datos
         id=int(input("Pon el id del cliente que quieres eliminar: "))
-        consulta="""DELETE FROM clientes WHERE idcliente = %s"""
-        cursor.execute(consulta(id,))
+        consulta="""DELETE FROM cliente WHERE id_cliente = %s"""
+        cursor.execute(consulta,(id,))
         conexion.commit()
         if cursor.rowcount > 0:
             print(f"El cliente con ID {id} ha sido eliminada.")
@@ -79,16 +79,17 @@ def registrar_entrenador(conexion): # Creamos la funcion para registrar entrenad
         nombre=input("Escribe el nombre del nuevo entrenador")
         especialidad=input("Introduce la especialidad del nuevo entrenador")
         consulta="""INSERT INTO entrenadores VALUES(%s,%s,%s)""" # Comando que usaremos para que se registre un entrenador en la tabla entrenador
-        cursor.execute(consulta(id,nombre,especialidad,)) # Ejecucion de consulta con los datos asignaados
-        conexion.comit()
+        cursor.execute(consulta,(id,nombre,especialidad,)) # Ejecucion de consulta con los datos asignaados
+        conexion.commit()
         print(f"el entrenador {nombre} se a registrado correctamente") # Print de que el entrenador se a registrado correctamente
+        cursor.close()
     except mysql.connector.Error as err:
         print(f"Error al registrar el entrenador: {err}") # Mensage en el que te dice el error que ocurre
 
 def listar_entrenadores(conexion): # Creamos la funcion para listar a los entrenadores
     cursor=conexion.cursor()
     consulta="""SELECT * FROM entrenadores""" # Comando que usaremos para listar la tabla entrenadores
-    cursor.execute(consulta) # Ejecucion del comando
+    cursor.execute(consulta,) # Ejecucion del comando
     resultados= cursor.fetchall()
     print("\n--- Entrenadores ---")
     for fila in resultados:
@@ -99,7 +100,7 @@ def eliminar_entrenador(conexion): # Creamos la funcion par eliminar entrenadore
     try: # Ponemos un try para la localizacion de errores
         cursor=conexion.cursor()# cursor para conectar a la base de datos
         id=int(input("Pon la id del entrenador que desees eliminar: "))
-        consulta="""DELETE FROM entrenadores WHERE identrenador = %s""" # Ponemos el comando que ejecutaremos para eliminar al entrenador 
+        consulta="""DELETE FROM entrenadores WHERE id_entrenador = %s""" # Ponemos el comando que ejecutaremos para eliminar al entrenador 
         cursor.execute(consulta,(id,)) # Ejecutamos el comando y sus datos ya establecidos
         if cursor.rowcount > 0:
             print(f"El entrenador con ID {id} ha sido eliminada.")
@@ -109,7 +110,7 @@ def eliminar_entrenador(conexion): # Creamos la funcion par eliminar entrenadore
     except mysql.connector.Error as err:
         print(f"Error al eliminar al entrenador: {err}")# Mensage en el que te dice el error que ocurre
         
-def crear_actividades(conexion): # Creamos la funcion para crear actividades
+def registrar_actividades(conexion): # Creamos la funcion para registrar una actividad
     try: # Ponemos un try para localizar errores
         cursor=conexion.cursor() # cursor para conectar a la base de datos
         id=int(input("Introduce el id de la actividad: "))
@@ -117,9 +118,84 @@ def crear_actividades(conexion): # Creamos la funcion para crear actividades
         horario=input("introduce el horario del la actividad: ")
         duracion=input("introduce la duracion de la actividad: ")
         identrenador=int(input("introduce la ip de in entrenador: "))
-        cursor.execute("SELECT COUNT(*) FROM pedido WHERE identrenador = %s", (identrenador,)) # Ejecutamos el sigliente comando para comprobar que exixiste algun entrenador con el id puesto
+        cursor.execute("SELECT COUNT(*) FROM entrenadores WHERE id_entrenador = %s", (identrenador,)) # Ejecutamos el sigliente comando para comprobar que exixiste algun entrenador con el id puesto
         if cursor.fetchone()[0] == 0:  # Si no hay un entrenador con ese ID, muestra un error
             print(f"No existe un entrenador con ID {identrenador}.")
             return
+        consulta="""INSERT INTO actividades VALUES(%s,%s,%s,%s,%s)""" # Ponemos el comando que ejecutaremos para registrar una actividad
+        cursor.execute(consulta,(id,nombre,horario,duracion,identrenador,)) # Ejecutamos consulta con sus datos
+        conexion.commit()
+        print(f"Se a regirtrado correctamente la actividad {nombre}") # Mensage de que el registro se a hecho correctamente
+        cursor.close()
     except mysql.connector.Error as err:
         print(f"Error al crear la actividad: {err}") # Mensage en el que te dice el error que ocurre
+        
+def listar_actividades(conexion): # Creamos la funcion para listar las actividades
+    cursor=conexion.cursor()
+    consulta="""SELECT * FROM actividades""" # comando que usaremos para listar actividades
+    cursor.execute(consulta,) # Ejecutamos consulta
+    resultados= cursor.fetchall()
+    print("\n--- Actividades ---")
+    for fila in resultados:
+        print(f"ID: {fila[0]}, Nombre: {fila[1]}, Horarios: {fila[2]}, Duracion: {fila[3]}, Identrenador: {fila[4]}")
+    cursor.close()
+    
+def eliminar_actividades(conexion): # Creamos una funcion para eliminar actividades
+    try:# ponemos un try para la detecion de errores
+        cursor=conexion.cursor()
+        id=int(input("introduce el id de la actividad que quieres eliminar: "))
+        consulta="""DELETE FROM actividades WHERE id_actividad = %s""" # Comando que usaremos para eliminar una actividad
+        cursor.execute(consulta,(id,)) # Ejecutamos el consulta y sus datos
+        if cursor.rowcount > 0:
+            print(f"El actividad con ID {id} ha sido eliminada.")
+        else:
+            print(f"No se encontró ninguna actividad con ID {id}.")
+        cursor.close()
+    except mysql.connector.Error as err: 
+        print(f"error al eliminar la actividad: {err}") # Mensage en el que nos dice el error ocurido
+
+def registrar_inscripciones(conexion): # Creamos una funcion para registrar una inscripcion
+    try: #ponemos un try para la detecion de errores
+        cursor=conexion.cursor()
+        id=int(input("introduce sl id del inscripcion que quieres registrar: "))
+        idcliente=int(input("introduce el id del cliente que quieres escribir: "))
+        cursor.execute("SELECT COUNT(*) FROM cliente WHERE id_cliente = %s", (idcliente,)) # Ejecutamos el sigliente comando para comprobar que exixiste algun cliente con el id puesto
+        if cursor.fetchone()[0] == 0:  # Si no hay un cliente con ese ID, muestra un error
+            print(f"No existe un cliente con ID {idcliente}.")
+            return
+        idactividad=int(input("introduce el id de la actividad: "))
+        cursor.execute("SELECT COUNT(*) FROM actividades WHERE id_actividad = %s", (idactividad,)) # Ejecutamos el sigliente comando para comprobar que exixiste alguna actividad con el id puesto
+        if cursor.fetchone()[0] == 0:  # Si no hay una actividad con ese ID, muestra un error
+            print(f"No existe una actividad con ID {idactividad}.")
+            return
+        consulta="""INSERT INTO inscripciones VALUES(%s,%s,%s)""" # Ponemos el comando que ejecutaremos para registrar una inscripcion
+        cursor.execute(consulta,(id,idcliente,idactividad))
+        conexion.commit()
+        print("la inscripcion se a creado correctamente") # Mensage para comprobar que la inscripcion se a creado
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(f"error al registrar la inscripcion: {err}") # Mensage en el que nos dice el error ocurido
+
+def listar_inscripcion(conexion): # Creamos una funcion para listar las inscripciones
+    cursor=conexion.cursor()
+    consulta="""SELECT * FROM inscripciones""" # Comando que usaremos para listar las inscripciones
+    cursor.execute(consulta,) # Ejecutamos consulta
+    resultados= cursor.fetchall()
+    print("\n--- inscripciones ---")
+    for fila in resultados:
+        print(f"ID: {fila[0]}, Id_Cliente: {fila[1]}, ID_Actividades: {fila[2]}")
+    cursor.close()
+
+def eliminar_inscripcion(conexion): # Creamos una funcion para listar las inscripciones
+    try: # ponemos un try para la resolucion de errores
+        cursor=conexion.cursor()
+        id=int(input("introduce el id de la inscripcion que desees eliminar: "))
+        consulta="""DELETE FROM inscripciones WHERE id_inscripcion = %s""" # comando que usaremos la la eliminacion de la inscripcion
+        cursor.execute(consulta,(id,)) # ejecutamos consulta con los datos
+        if cursor.rowcount > 0:
+            print(f"La actividad con ID {id} ha sido eliminada.")
+        else:
+            print(f"No se encontró ninguna inscripcion con ID {id}.")
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(f"error al eliminar la inscripcion: {err}") # Mensaje que nos dice el error ocurido
--- ejercicio19/inicio.py
+++ ejercicio19/inicio.py
@@ -5,10 +5,10 @@ import funciones # Importamos funciones.py
 conexion=funciones.conectar("centro_deportivo") # Nos conetamos a la base de datos centro_deportivo
 
 while True: # Creamos un while true para la gestion de la base de datos
-    funciones.menu_principal # Invocamos la funcion menu_principal
+    funciones.menu_principal() # Invocamos la funcion menu_principal
     opcion=int(input("Selecione la opcion que quieras hacer: "))
     if opcion==1: # Elegimos la tabla cliente
-        funciones.menu_operaciones # Invocamos la funcion menu_operaciones
+        funciones.menu_operaciones() # Invocamos la funcion menu_operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
         if elecion==1:
             funciones.registrar_cliente(conexion) # Invocamos la funcion registrar_cliente
@@ -18,15 +18,39 @@ while True: # Creamos un while true para la gestion de la base de datos
             funciones.eliminar_clientes(conexion) # Invocamos la funcion eliminar_clientes
         elif elecion==4:
             break
-    elif opcion==2: # Elegimos la tabla actividades
-        funciones.menu_operaciones # Invocamos la funcion menu_operaciones
+    elif opcion==2: # Elegimos la tabla entrenadores
+        funciones.menu_operaciones() # Invocamos la funcion menu_operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
-    elif opcion==3: # Elegimos la tabla entrenadores
-        funciones.menu_operaciones # Invocamos la funcion menu_operaciones
+        if elecion==1:
+            funciones.registrar_entrenador(conexion) # Invocamos la funcion registrar_entrenador
+        elif elecion==2:
+            funciones.listar_entrenadores(conexion) # Invocamos la funcion listar_entrenadores
+        elif elecion==3:
+            funciones.eliminar_entrenador(conexion) # Invocamos la funcion eliminar_entrenador
+        elif elecion==4:
+            break
+    elif opcion==3: # Elegimos la tabla actividades
+        funciones.menu_operaciones() # Invocamos la funcion menu_operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
+        if elecion==1:
+            funciones.registrar_actividades(conexion) # Invocamos la funcion registrar_actividades
+        elif elecion==2:
+            funciones.listar_actividades(conexion) # Invocamos la funcion listar_actividades
+        elif elecion==3:
+            funciones.eliminar_actividades(conexion) # Invocamos la funcion eliminar_actividades
+        elif elecion==4:
+            break
     elif opcion==4: # Elegimos la tabla inscripcion
-        funciones.menu_operaciones # Invocamos la funcion menu_operaciones
+        funciones.menu_operaciones() # Invocamos la funcion menu_operaciones
         elecion=int(input("introduce la operacion que quierres haces"))
+        if elecion==1:
+            funciones.registrar_inscripciones(conexion)
+        elif elecion==2:
+            funciones.listar_inscripcion(conexion)
+        elif elecion==3:
+            funciones.eliminar_inscripcion(conexion)
+        elif elecion==4:
+            break
     elif opcion==5: # Elegimos salir de la base de datos
         conexion.close
         break
