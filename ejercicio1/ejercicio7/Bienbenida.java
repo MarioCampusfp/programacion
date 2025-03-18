605948845a2b5eba655e2d4225017b0ffb4bad97
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2025-03-18 20:27:18 +0100

    ejercicio1

---
+++ ejercicio1/ejercicio7/Bienbenida.java
@@ -0,0 +1,5 @@
+public class Bienbenida {
+	public void mostrarSaludo(String nombre, int edad) {
+		System.out.println( "Hola" + nombre + ", tienes" + edad + " años.");
+	}
+}
---
+++ ejercicio1/ejercicio7/Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+	public static void main(String[] args){
+		Bienbenida hola = new Bienbenida();
+		hola.mostrarSaludo("Mario del Rey", 23);
+	}
+}
