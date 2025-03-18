7bdb01b8bee99636d2a7bb326f3f899cf8efd464
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2025-03-18 09:39:26 +0100

    ejercicios ejemplo

---
+++ java/ejercicio.03/MiNombre.java
@@ -0,0 +1,11 @@
+public class MiNombre {
+	public static void main(String[] args) {
+		System.out.println("Mario del Rey");
+		MiNombre hola= new MiNombre();
+		hola.saludar();
+		
+	}
+	public void saludar() {
+		System.out.println("Bienvenido a Java");
+	}
+}
