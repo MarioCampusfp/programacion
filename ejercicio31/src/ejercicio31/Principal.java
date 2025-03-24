a1d6c4fbccbea0adee454cbdc3906453044b1224
MarioCampusfp <mario.delreymoreno24@campusfp.es>
2025-03-24 14:44:56 +0100

    ejercicio 3 java

---
+++ ejercicio31/src/ejercicio31/Principal.java
@@ -0,0 +1,19 @@
+package ejercicio31;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona paco = new Persona();
+		paco.nombre = "paco";
+		paco.edad = 46;
+		paco.mostrarDatos();
+		
+		Persona jorge = new Persona();
+		jorge.nombre= "jorge";
+		jorge.edad= 29;
+		jorge.mostrarDatos();
+
+	}
+
+}
